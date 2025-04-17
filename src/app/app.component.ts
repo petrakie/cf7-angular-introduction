d8dc237c14cb596ef9c824b173089b7b84f52bfe
Evangelia Petraki <petrakie@icloud.com>
2025-04-17 16:27:29 +0200

    New component person table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Evangelia';
+
+  person = {
+    givenName: 'Evangelia',
+    surName: 'Petraki',
+    age: 29,
+    email: "petrakie@icloud.com"
+  }
 }
